6e94eb559dbe979000d0ca517a47e35ea57c18c8
deleteLater <mikcczhang@gmail.com>
2020-05-02 09:42:57 +0800

    ng add @angular/material

---
+++ frontend/src/app/modules/app-material.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+const materials = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+];
+
+@NgModule({
+  imports: [materials],
+  exports: [materials]
+})
+export class AppMaterialModule {
+}
