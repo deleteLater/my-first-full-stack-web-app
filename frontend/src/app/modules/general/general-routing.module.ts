b7b881d451c30234dec3dd74cdcd7949ddb8c9d8
deleteLater <mikcczhang@gmail.com>
2020-05-06 15:52:32 +0800

    add user,role,permission template in general module

--- frontend/src/app/modules/general/general-routing.module.ts
+++ frontend/src/app/modules/general/general-routing.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { GeneralComponent } from './general.component';
+import {GeneralComponent} from './general.component';
 
-const routes: Routes = [{ path: '', component: GeneralComponent }];
+const routes: Routes = [{path: '', component: GeneralComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class GeneralRoutingModule { }
+export class GeneralRoutingModule {
+}
--- frontend/src/app/modules/general/general.module.ts
+++ frontend/src/app/modules/general/general.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { GeneralRoutingModule } from './general-routing.module';
-import { GeneralComponent } from './general.component';
+import {GeneralRoutingModule} from './general-routing.module';
+import {GeneralComponent} from './general.component';
 import {GeneralMaterialModule} from '../material/general-material.module';
+import {UserComponent} from './user/user.component';
+import {RoleComponent} from './role/role.component';
+import {PermissionComponent} from './permission/permission.component';
 
 @NgModule({
-  declarations: [GeneralComponent],
+  declarations: [GeneralComponent, UserComponent, RoleComponent, PermissionComponent],
   imports: [
     CommonModule,
     GeneralRoutingModule,
     GeneralMaterialModule
   ]
 })
-export class GeneralModule { }
+export class GeneralModule {
+}
