2a727786ec79f8325f4fd2c32551ed5cb47c31ed
deleteLater <mikcczhang@gmail.com>
2020-05-08 22:25:00 +0800

    fix bug (when value <= maxLength) should return value directly

--- frontend/src/app/shared/pipes/string-truncate.pipe.ts
+++ frontend/src/app/shared/pipes/string-truncate.pipe.ts
@@ -6,7 +6,11 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class StringTruncatePipe implements PipeTransform {
 
   transform(value: string, maxLength = 25): string {
+
+    if (value.length <= maxLength) {
+      return value;
+    }
+
     return value.slice(0, maxLength) + '...';
   }
-
 }
