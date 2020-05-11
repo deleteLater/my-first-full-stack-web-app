b930dc893caecd493f40890e0d58b5bd9c3bb3d1
deleteLater <mikcczhang@gmail.com>
2020-05-11 21:23:12 +0800

    add account & common-user controller

---
+++ backend/src/SaaSApplicationManagement.HttpApi/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SaaSApplicationManagement.Models;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace SaaSApplicationManagement.Controllers
+{
+    [ControllerName("Account")]
+    [Route("api/account")]
+    public class AccountController : AbpController
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(
+            IHttpClientFactory clientFactory,
+            IConfiguration configuration
+        )
+        {
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+        }
+        
+        /// <summary>
+        /// user login
+        /// </summary>
+        [Route("login")]
+        [HttpPost]
+        public async Task<ActionResult> AccountLoginAsync([FromBody] LoginInput input)
+        {
+            var client = _clientFactory.CreateClient();
+
+            // 从 IdentityServer 服务器请求 AccessToken。
+            var tokenResponse = await client.RequestPasswordTokenAsync(
+                new PasswordTokenRequest
+                {
+                    Address = await RetrieveTokenPointEndAsync(),
+                    ClientId = _configuration["IdentityServer:WebClientId"],
+                    ClientSecret = _configuration["IdentityServer:WebClientSecret"],
+                    UserName = input.Name,
+                    Password = input.Password,
+                    Scope = _configuration["IdentityServer:ApiName"]
+                }
+            );
+
+            return tokenResponse.IsError
+                ? throw new UserFriendlyException(tokenResponse.ErrorDescription)
+                : Content(tokenResponse.Json.ToString(), "application/json; encoding=utf-8");
+        }
+        
+        private async Task<string> RetrieveTokenPointEndAsync()
+        {
+            var client = _clientFactory.CreateClient();
+
+            // discover endpoints from metadata
+            var disco = await client.GetDiscoveryDocumentAsync(_configuration["IdentityServer:AuthorityServer"]);
+            if (disco.IsError)
+            {
+                throw new HttpRequestException($"请求 Identity Server 4 终端点失败。{disco.Error}");
+            }
+
+            return disco.TokenEndpoint;
+        }
+    }
+}
---
+++ backend/src/SaaSApplicationManagement.HttpApi/Controllers/CommonUserController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SaaSApplicationManagement.Users;
+using SaaSApplicationManagement.Users.Dtos;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace SaaSApplicationManagement.Controllers
+{
+    [ControllerName("CommonUser")]
+    [Route("api/common-user")]
+    public class CommonUserController : AbpController, ICommonUserAppService
+    {
+        private readonly ICommonUserAppService _service;
+
+        public CommonUserController(ICommonUserAppService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// create new common user
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<CommonUserDto> CreateAsync([FromBody] CreateCommonUserDto input)
+        {
+            return await _service.CreateAsync(input);
+        }
+
+        /// <summary>
+        /// get common user paged-list
+        /// </summary>
+        /// <param name="filter">page params</param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<PagedResultDto<CommonUserDto>> GetListAsync(CommonUserFilter filter)
+        {
+            return await _service.GetListAsync(filter);
+        }
+
+        /// <summary>
+        /// delete a specific common user
+        /// </summary>
+        /// <param name="id">common user id</param>
+        /// <returns></returns>
+        [HttpDelete]
+        public async Task DeleteAsync(long id)
+        {
+            await _service.DeleteAsync(id);
+        }
+    }
+}
