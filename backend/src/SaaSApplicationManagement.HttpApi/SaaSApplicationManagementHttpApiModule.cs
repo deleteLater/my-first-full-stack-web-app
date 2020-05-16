187e8cf90001378eb62ec789745565a24fcdfba9
deleteLater <mikcczhang@gmail.com>
2020-05-16 22:20:01 +0800

    add swagger-jwt-authorization

--- backend/src/SaaSApplicationManagement.HttpApi/SaaSApplicationManagementHttpApiModule.cs
+++ backend/src/SaaSApplicationManagement.HttpApi/SaaSApplicationManagementHttpApiModule.cs
@@ -105,6 +105,30 @@ namespace SaaSApplicationManagement
                             "XmlDoc/HttpApi.xml"
                         )
                     );
+
+                    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "JWT Authorization header using the Bearer scheme."
+                    });
+                    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] { }
+                        }
+                    });
                 });
         }
 
