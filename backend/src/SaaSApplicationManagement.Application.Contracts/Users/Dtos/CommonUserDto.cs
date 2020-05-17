073fe9ebc4d632babc5e60f73c808e37947d0269
deleteLater <mikcczhang@gmail.com>
2020-05-17 11:23:16 +0800

    fix phone-number not mapping

--- backend/src/SaaSApplicationManagement.Application.Contracts/Users/Dtos/CommonUserDto.cs
+++ backend/src/SaaSApplicationManagement.Application.Contracts/Users/Dtos/CommonUserDto.cs
@@ -4,7 +4,7 @@
     {
         public string Name { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public string Phone { get; set; }
 
         public string Email { get; set; }
 
